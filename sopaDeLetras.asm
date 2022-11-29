8a6202d208511bbec0ea0acd3e52d42b39fb18a3
RobertoPatino1 <rapatino@espol.edu.ec>
2022-11-29 18:20:54 -0500

    Se agrego el manejo para el ingreso de un dato erroneo

--- sopaDeLetras.asm
+++ sopaDeLetras.asm
@@ -7,6 +7,9 @@ msgSeleccion1 db 'Selecciona el numero de una categoria para generar la sopa de 
 msgSeleccion2 db '1. Equipos clasificados al mundial de futbol 2022 $'
 msgSeleccion3 db '2. Disciplinas deportivas $'
 msgSeleccion4 db 'Ingresa una opcion: $'
+
+msgErrorCategoria db 'Ha ingresado una opcion no valida, por favor intente ingresar una opcion nuevamente. '
+
 salto DB 13,10,"$" ;INSTRUCCION DE SALTO DE LINEA
 msgContinuar db 'Deseas continuar jugando? (1=Si/0=No) $'
 
@@ -49,15 +52,52 @@ iniciarJuego:
     ;Salto de linea        
     lea dx, salto        
     int 21h 
-    int 21h
+    int 21h    
+    jmp ingresoCategoria
+
+
+ingresoCategoria:   ;Rutina que permite al usuario seleccionar una categoria para generar la sopa de letras
     ;Mensaje de seleccion 4
     lea dx,msgSeleccion4
-    int 21h                
-    
-    
+    int 21h
     ;Capturando datos ingresados por el usuario
     mov ah, 01h         ;funcion para captura de dato
     int 21h             ;interrupcion para captura de dato (ASCCI), se almacena en al
     sub al, 30h         ;convertir codigo ASCII capturado al valor ingresado por el usuario
-    mov seleccionCategoria, al     
+    mov seleccionCategoria, al
+    ;Salto de linea
+    mov ah,09h        
+    lea dx, salto        
+    int 21h    
+    jmp validarIngresoCategoria
     
+    
+
+validarIngresoCategoria:    ;Rutina que valida que el numero ingresado para la categoria sea valido
+    cmp seleccionCategoria,1
+    jz iniciarCategoriaMundial  ;Se selecciono Equipos de Futbol
+    
+    cmp seleccionCategoria,2
+    jz iniciarCategoriaDeportes ;Se selecciono Deportes
+    
+    jnz ingresoCategoriaErroneo ;Se ingreso una opcion no valida
+    
+      
+ingresoCategoriaErroneo:    ;Rutina para manejar un ingreso de categoria invalido
+    ;Mensaje de inicio
+    mov ah,09h
+    lea dx,msgErrorCategoria
+    int 21h
+    ;Salto de linea        
+    lea dx, salto        
+    int 21h
+    int 21h
+    jmp ingresoCategoria    ;Volvemos a pedir ingreso
+    
+    
+    
+iniciarCategoriaMundial:
+
+
+
+iniciarCategoriaDeportes:
